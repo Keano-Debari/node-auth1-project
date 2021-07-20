c091dc264835744154d2fe7e361d8fb8d5d0d114
unknown <Keano@Firelink.Firelink>
2021-07-19 18:20:27 -0700

    middlewares done

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,11 @@
 // Require `checkUsernameFree`, `checkUsernameExists` and `checkPasswordLength`
 // middleware functions from `auth-middleware.js`. You will need them here!
-
+const router = require('express').Router()
+const {
+  checkUsernameFree,
+  checkUsernameExists,
+  checkPasswordLength
+} = require('./auth-middleware')
 
 /**
   1 [POST] /api/auth/register { "username": "sue", "password": "1234" }
@@ -25,6 +30,10 @@
   }
  */
 
+  router.post('/register', checkPasswordLength, checkUsernameFree, (req, res, next) => {
+    res.json('register')
+  })
+
 
 /**
   2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
@@ -42,6 +51,11 @@
   }
  */
 
+  router.post('/login', checkUsernameExists, (req, res, next) => {
+    res.json('login')
+  })
+
+
 
 /**
   3 [GET] /api/auth/logout
@@ -58,6 +72,11 @@
     "message": "no session"
   }
  */
+
+  router.get('/logout', (req, res, next) => {
+    res.json('logout')
+  })
+
 
  
 // Don't forget to add the router to the `exports` object so it can be required in other modules
